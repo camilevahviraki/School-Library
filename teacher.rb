62a5959e29862af0821fcac660123e82802c273a
camilevahviraki <camilevahviraki@gmail.com>
2022-07-22 22:57:57 +0200

    Finish updating

--- teacher.rb
+++ teacher.rb
@@ -1,10 +1,25 @@
-module Teacher
-  def initialize(specialization, age, parent_permission: true, name: 'Unknown')
-    super(name, age, parent_permission)
+require_relative './person'
+class Teacher < Person
+  @@teachers = []
+  def initialize(specialization, age, name, profession)
+    super(age, name, profession)
     @specialization = specialization
+    @@teachers.push(
+      {
+        'name' => name,
+        'specialization' => specialization,
+        'age' => age
+      }
+    )
   end
 
   def can_use_service?
     true
+  end
+
+  def teachers_list
+    @@teachers.each_with_index do |teacher, idx|
+      puts "#{idx} || name: #{teacher['name']}, age: #{teacher['age']}, specialization: #{teacher['specialization']}"
+    end
   end
 end
