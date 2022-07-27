ae8a3d42ddd9043f134b32d6bf58c6eb7e0ac0f9
SimbaWANGU <simbawangulu@gmail.com>
2022-07-27 16:31:32 +0300

    add unit tests for classes and methods

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,6 +2,11 @@ require_relative '../student'
 
 describe Student do 
   context "When testing the Student class" do 
+    it "should show a student object is an instance of student class" do
+      st = Student.new("Form1Q", 24, "Simba", "Student", true)
+      expect(st).to be_instance_of Student 
+    end
+
     it "should say '¯\(ツ)/¯' when we call the play_hooky method" do 
       st = Student.new("Form1Q", 24, "Simba", "Student", true)
       message = st.play_hooky 
